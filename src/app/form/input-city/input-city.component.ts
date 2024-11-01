c6f0b6aa4081f3ac06aa3536b99c13ba3fd326f9
artemian4ik <36512934+artemian4ik@users.noreply.github.com>
2024-11-01 10:44:07 +0200

    add form input city

---
+++ src/app/form/input-city/input-city.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-city',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-city.component.html',
+  styleUrl: './input-city.component.scss'
+})
+export class InputCityComponent {
+
+}
